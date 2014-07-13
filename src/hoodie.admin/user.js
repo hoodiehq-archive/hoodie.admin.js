81ef5d1f4ba3a3e13164673210b8e16a43f39a1e
Gregor Martynus <gregor@martynus.net>
2014-07-13 20:11:54 +0200

    feat: hoodieAdmin.user.search

--- src/hoodie.admin/user.js
+++ src/hoodie.admin/user.js
@@ -1,7 +1,24 @@
 function hoodieAdminUser(hoodieAdmin) {
-  hoodieAdmin.user = hoodieAdmin.open('_users', {
-    prefix: 'org.couchdb.user:'
+  var PREFIX = 'org.couchdb.user:';
+  var user = hoodieAdmin.open('_users', {
+    prefix: PREFIX
   });
+
+  user.search = function search(query) {
+    var userPrefix = PREFIX + 'user/';
+    var startkey = encodeURIComponent(userPrefix + query);
+    var endkey = encodeURIComponent(userPrefix + query + '|');
+    return hoodieAdmin.user.request('GET', '/_all_docs?include_docs=true&startkey="'+startkey+'"&endkey="'+endkey+'"')
+    .then(function(response){
+      return response.rows.map(function(row) { 
+        var doc = row.doc;
+        doc.id = doc.name.split('/').pop();
+        return doc;
+      });
+    })
+  };
+
+  hoodieAdmin.user = user;
 }
 
 module.exports = hoodieAdminUser;
